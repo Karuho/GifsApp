6ef5010cc9957faa155f8259a402b11abd430923
Carlos Torres <carlos.to.fa87@gmail.com>
2025-04-19 16:49:48 -0400

    Update changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    companyName: 'Gifs',
+    companyName2: 'App',
+    companySlogan: 'Maneja tus gifts',
+
+    // API Keys
+    giphyApiKey: 'o1RgkVC5666ESFa5V2SBlByxma199b1c',
+    giphyUrl: 'https://api.giphy.com/v1',
+
+
+    // URLS
+};
