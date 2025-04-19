0e8b42bbb7de3b3f9c2c6feb7b7edee0978fafe6
Carlos Torres <carlos.to.fa87@gmail.com>
2025-04-19 17:12:26 -0400

    Update changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,11 @@
+import { secretEnvironment } from "./environment.secret";
+
 export const environment = {
     production: true,
     companyName: 'Gifs',
     companyName2: 'App',
     companySlogan: 'Maneja tus gifts',
-
-    // API Keys
-    giphyApiKey: 'o1RgkVC5666ESFa5V2SBlByxma199b1c',
-    giphyUrl: 'https://api.giphy.com/v1',
+    giphyApiKey: secretEnvironment.giphyApiKey
 
 
     // URLS
